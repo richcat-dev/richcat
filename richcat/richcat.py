9ed085029d8a992162a2c1024f139857fd33a72f
yamamoto-yuta <automatuX78@gmail.com>
2020-11-22 03:34:34 +0900

    [Update] Split main() to some functions

--- richcat/richcat.py
+++ richcat/richcat.py
@@ -1,11 +1,110 @@
 from __future__ import unicode_literals
 import os
 import argparse
-import pandas as pd
+
 from rich.console import Console
 from rich.markdown import Markdown
 from rich.syntax import Syntax
 from rich.table import Table
+
+
+def decide_text_width(arg_width):
+    """
+    The function which decide text width.
+
+    Parameters
+    ----------
+    arg_width : str
+        command line argument of width
+
+    Returns
+    -------
+    : int
+        target width
+    """
+    target_width = float(arg_width)
+    if target_width <= 1.0:
+        _, terminal_width = os.popen('stty size', 'r').read().split()
+        terminal_width = float(terminal_width)
+        return int(terminal_width*target_width)
+    else:
+        return int(target_width)
+
+
+def infer_filetype(filepath, filetype):
+    """
+    The function which infer file type
+
+    Parameters
+    ----------
+    filepath : str
+        command line argument of filepath
+    filetype : str
+        command line argument of filetype
+
+    Returns
+    -------
+    filepath : str
+        filepath
+    filetype : str
+        filetype
+    """
+    if filetype == 'auto':
+        return filepath, filepath.split('.')[-1]
+    else:
+        return filepath, filetype
+
+
+def make_rich(filepath, filetype):
+    """
+    The function which make rich text
+
+    Parameters
+    ----------
+    filepath : str
+        filepath
+    filetype : str
+        filetype
+
+    Returns
+    -------
+    text : *
+        rich text
+    """
+
+    syntax_dict = {
+        'py': 'python',
+        'rust': 'rust',
+        'js': 'javascript',
+        'tex': 'tex'
+    }
+    if filetype in syntax_dict.keys():
+        with open(filepath) as f:
+            text = f.read()
+        return Syntax(text, syntax_dict[filetype], line_numbers=True)
+
+    if filetype == 'md':
+        with open(filepath) as f:
+            text = f.read()
+        return Markdown(text)
+
+    if filetype == 'csv':
+        # instance
+        text = Table(show_header=True, header_style="bold magenta")
+        # Read file
+        lst_table = []
+        with open(filepath) as f:
+            for l in f.read().splitlines():
+                lst_table.append(l.split(','))
+        # Generate rich text
+        columns = []
+        for col in range(len(lst_table[0])):
+            text.add_column(str(col))
+            columns += [col]
+        for row in lst_table:
+            text.add_row(*row)
+        return text
+
 
 def main():
     """ Args """
@@ -13,54 +112,21 @@ def main():
     parser.add_argument('filepath', type=str, metavar='FilePath', help='file path')
     parser.add_argument('-t', '--filetype', type=str, nargs='?', default='auto', metavar='FileType', help='filetype')
     parser.add_argument('-w', '--width', type=str, nargs='?', default='1.0', metavar='Width', help='width')
-    parser.add_argument('-c', '--color_system', type=str, nargs='?', default='256', choices=['standard', '256', 'truecolor', 'windows'],metavar='Width', help='width')
+    parser.add_argument('-c', '--color_system', type=str, nargs='?', default='256', choices=['standard', '256', 'truecolor', 'windows'], metavar='Width', help='width')
     args = parser.parse_args()
 
     """ Deciding TextWidth """
-    target_width = float(args.width)
-    if target_width<=1.0:
-        _, terminal_width = os.popen('stty size', 'r').read().split()
-        terminal_width    = float(terminal_width)
-        target_width      = int(terminal_width*target_width)
-    else:
-        target_width = int(target_width)
+    target_width = decide_text_width(args.width)
 
     """ Infering FileType """
-    filetype=args.filetype
-    filepath=args.filepath
-    if filetype=='auto':
-        filetype=filepath.split('.')[-1]
+    filepath, filetype = infer_filetype(args.filepath, args.filetype)
 
     """ General Preparing """
     console = Console(color_system=args.color_system, width=target_width)
-    if filetype=='csv':
-        df = pd.read_csv(filepath, header=None)
-    else:
-        with open(filepath) as f:
-            text = f.read()
 
     """ Make Rich """
-    syntax_dict = {
-            'py': 'python',
-            'rust': 'rust',
-            'js': 'javascript',
-            'tex': 'tex'
-            }
-    if filetype in syntax_dict.keys():
-        text = Syntax(text, syntax_dict[filetype], line_numbers=True)
-    if filetype=='md':
-        text = Markdown(text)
-    if filetype in ['csv', 'xlsx']:
-        text = Table(show_header=True, header_style="bold magenta")
-        columns = []
-        for col in df:
-            text.add_column(str(col))
-            columns += [col]
-        for index, row in df.iterrows():
-            row = [str(row[col]) for col in columns]
-            text.add_row(*row)
+    console.print(make_rich(filepath, filetype))
 
-    console.print(text)
 
 if __name__ == '__main__':
     main()
