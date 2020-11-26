7f08107b0a0ed44fb95ae874061d1eebafe629b4
yamamoto-yuta <automatuX78@gmail.com>
2020-11-26 14:59:29 +0900

    Update '__init__.py'

--- richcat/__init__.py
+++ richcat/__init__.py
@@ -1,7 +1,7 @@
-__copyright__    = 'Copyright (C) 2020 Shotaro Kataoka'
-__version__      = '0.0.0'
+__copyright__    = 'Copyright (C) 2020 Yuta Yamamoto, Shotaro Kataoka'
+__version__      = '0.1.0'
 __license__      = 'MIT'
-__author__       = 'Shotaro Kataoka'
-__author_email__ = 'shotaro.kata@gmail.com'
-__url__          = 'https://github.com/ShotaroKataoka/richcat'
+__author__       = 'Yuta Yamamoto, Shotaro Kataoka'
+__author_email__ = 'automatuX78@gmail.com'
+__url__          = 'https://github.com/richcat-dev/richcat'
 
