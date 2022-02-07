b3205f885a1d41a9aafdd59417f94bb00f88ec18
yamamoto-yuta <automatuX78@gmail.com>
2022-02-07 16:14:13 +0900

    💚 動作確認用の変更

--- richcat/__information__.py
+++ richcat/__information__.py
@@ -1,5 +1,5 @@
 __copyright__ = 'Copyright (C) 2020 Yuta Yamamoto, Shotaro Kataoka'
-__version__ = '0.4.2'
+__version__ = '0.5.0'
 __license__ = 'MIT'
 __author__ = 'Yuta Yamamoto, Shotaro Kataoka'
 __author_email__ = 'automatuX78@gmail.com'
